28a62588656d9573093d183619172c383a0c3be2
Andrey <erkcasb@gmail.com>
2020-11-08 20:57:46 +0500

    netology08112020

--- src/Homework/Main.java
+++ src/Homework/Main.java
@@ -1,59 +1,21 @@
 package Homework;
 
-import java.util.Random;
-import java.util.Scanner;
+
 
 
 public class Main {
-    public static final int SIZE = 8;
+
 
     public static void main(String[] args) {
 
-        int[][] colors = new int[SIZE][SIZE];
+        String s1 = "abc";
+        String s2 = s1;
+        s1 += "d";
+        System.out.println(s1 + " " + s2 + " " + (s1 == s2));
 
-        Random random = new Random();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                colors[i][j] = random.nextInt(256);
-            }
-        }
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", colors[i][j]);
-            }
+        StringBuffer sb1 = new StringBuffer("abc");
+        StringBuffer sb2 = sb1;
+        sb1.append("d");
+        System.out.println(sb1 + " " + sb2 + " " + (sb1 == sb2));
 
-            System.out.println();
-        }
-        int[][] rotateColors = new int[SIZE][SIZE];
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n");
-        System.out.println("Введите угол поворота на который необходимо повернуть матрицу 90/180/270 и нажмите <enter> ");
-        String cornerLine = scanner.nextLine();
-        if (cornerLine.equals("90")) {
-            rotateColors = turnToRight(colors);
-        }else if (cornerLine.equals("180")) {
-            rotateColors = turnToRight(colors);
-            rotateColors = turnToRight(rotateColors);
-        } else if (cornerLine.equals("270")) {
-            rotateColors = turnToRight(colors);
-            rotateColors = turnToRight(colors);
-            rotateColors = turnToRight(colors);
-        }
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", rotateColors[i][j]);
-            }
-            System.out.println();
-        }
-    }
-    public static int[][] turnToRight ( int[][] array){
-        int[][] resultArray = new int[array[0].length][array.length];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                resultArray[j][SIZE - i - 1] = array[i][j];
-            }
-        }
-        return resultArray;
-    }
-}
+    }}
